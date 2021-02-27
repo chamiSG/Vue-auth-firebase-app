f1c792497d6d2c5e923a60c3b3e394ecf5b84239
DESKTOP-TKBO95A\HADES <chamiSG>
2021-02-26 18:07:18 -0800

    init

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Dashboard</div>
+          <div class="card-body">
+            <div v-if="user" class="alert alert-success" role="alert">You are logged in!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    // map `this.user` to `this.$store.getters.user`
+    ...mapGetters({
+      user: "user"
+    })
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
